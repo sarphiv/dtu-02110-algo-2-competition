e6ae3cba5842c3f1693f26dea816640172199dbd
sarphiv <32750711+sarphiv@users.noreply.github.com>
2022-11-25 17:12:46 +0100

    Cleaned up tests for commit (had them for a while)

---
+++ run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for test_input in tests/*.in; do
+    if [[ $test_input =~ (.*)\.in ]]; then
+        OUTPUT=$(./main $test_input)
+        ANSWER=$(cat ${BASH_REMATCH[1]}.ans)
+        if [[ $OUTPUT == $ANSWER ]]; then
+            echo "✅ ${BASH_REMATCH[1]}"
+        else
+            echo -e "❌ ${BASH_REMATCH[1]}\t OUT:$OUTPUT\t != ANS:$ANSWER"
+        fi
+    else
+        echo "Bug in test script for path: $test_input"
+    fi
+done
